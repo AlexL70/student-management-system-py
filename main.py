f407723ab862a506d3d8a0181a45d0e9c1a1e0ee
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-10-30 19:15:54 +0200

    377. Creating a Table Structure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QPushButton, QMainWindow
+from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QPushButton, QMainWindow, QTableWidget
 import sys
 
 class MainWindow(QMainWindow):
@@ -11,6 +11,14 @@ class MainWindow(QMainWindow):
         addStudentAction = file_menu_item.addAction("Add Student")
         help_menu_item = self.menuBar().addMenu("&Help")
         aboutAction = help_menu_item.addAction("About")
+
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(["ID", "Name", "Course", "Mobile"])
+        self.setCentralWidget(self.table)
+
+    def load_data(self):
+        pass
 
 app = QApplication(sys.argv)
 main_window = MainWindow()
